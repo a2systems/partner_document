ed65352ac29685d2ae6f5ac9c622a05feb58fd68
mariogustavoorrillo <gustavo.orrillo@gmail.com>
2024-01-19 16:33:31 +0000

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2020 Brainbean Apps (https://brainbeanapps.com)
+# Copyright 2020-2022 CorporateHub (https://corporatehub.eu)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Partner Document",
+    "version": "16.0.1.0.1",
+    "category": "Partner",
+    "installable": True,
+    "application": False,
+    "summary": "Documents attached to the partner",
+    "depends": ["base"],
+    "data": [
+        "views/partner.xml", 
+        "views/ir_attachment.xml"],
+}
